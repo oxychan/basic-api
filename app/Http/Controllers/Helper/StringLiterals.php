a1d0e010b6edd19b62ca0cb1c370239468aeaa50
oxychan <anwrtaufik@gmail.com>
2022-09-20 07:35:08 +0700

    refactor routes

--- app/Http/Controllers/Helper/StringLiterals.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\Helper;
-
-class StringLiterals {
-    public static $productById = "/products/{id}";
-}
--- routes/api.php
+++ routes/api.php
@@ -17,18 +17,25 @@ use App\Http\Controllers\Helper\StringLiterals;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// StringLiterals::$productById -> "/products/{id}"
+
 // Public Routes
-Route::get('/products', [ProductController::class, 'index']);
-Route::get(StringLiterals::$productById, [ProductController::class, 'show']);
-Route::get('/products/search/{name}', [ProductController::class, 'search']);
+// Routes for products 
+Route::get('/products/search/{name}', [ProductController::class, 'search'])->name('products.search');
+Route::resource('/products', ProductController::class)->only(['index', 'show']);
+
+// Routes for register and login
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected Routes
+// Group of routes that guarded by auth sanctum
 Route::group(['middleware' => ['auth:sanctum']], function() {
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put(StringLiterals::$productById, [ProductController::class, 'update']);
-    Route::delete(StringLiterals::$productById, [ProductController::class, 'destroy']);
+    // Routes for products
+    Route::resource('/products', ProductController::class)->only(['store', 'update', 'destroy']);
     Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+// Will be execute when no routes matches
+Route::fallback(function() {
+    abort(404);
 });
